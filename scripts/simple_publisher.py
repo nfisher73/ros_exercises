a3d89ccaea35224dd26b9aa1c12eed400b14ade6
Noah Fisher <nfisher7@mit.edu>
2022-02-14 16:17:10 -0500

    Built base of Simple Publisher

---
+++ scripts/simple_publisher.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import rospy
+
+from random import uniform
+from std_msgs.msg import Float32
+
+def talker():
+    pub = rospy.Publisher('my_random_float', Float32) # topic: my_random_float, type: Float32
+    rospy.init_node('simple_publisher', anonymous=True) # Publish to simple_publisher
+    rate = rospy.Rate(20) # 20 hz
+    while not rospy.is_shutdown():
+        rando = random.uniform(0.0, 10.0) #generate random float from 0-10.0
+        rospy.loginfo("Number Generated: %s", rando)
+        pub.publish(rando)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
