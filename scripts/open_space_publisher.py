19338eafd15254c043aa1f2c6e64f955caa3b280
Noah Fisher <nfisher7@mit.edu>
2022-02-15 11:16:23 +0000

    Q5 Donezo

--- scripts/open_space_publisher.py
+++ scripts/open_space_publisher.py
@@ -3,20 +3,20 @@
 import rospy
 
 from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Float32
+from ros_exercises.msg import OpenSpace
 from numpy import pi
 
-pubD = rospy.Publisher("open_space/distance", Float32, queue_size=20)
-pubA = rospy.Publisher("open_space/angle", Float32, queue_size=20)
-
+pub = rospy.Publisher("open_space", OpenSpace, queue_size=20)
 
 def callback(scan):
     longest = max(scan.ranges) #longest range value
     index = scan.ranges.index(longest) #index of longest value
     angle = scan.angle_min + index*scan.angle_increment #angle at largest value calculated using min_angle, index, and angle increment
     
-    pubD.publish(longest)
-    pubA.publish(angle)
+    custom = OpenSpace()
+    custom.angle = angle
+    custom.distance = longest
+    pub.publish(custom)
 
 
 def listen():
