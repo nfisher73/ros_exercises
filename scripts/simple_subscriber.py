7b1a0e1145f63f1296ef16def5f93755a130562e
Noah Fisher <nfisher7@mit.edu>
2022-02-14 20:20:28 -0500

    Q2 Donezo

---
+++ scripts/simple_subscriber.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import rospy
+
+from std_msgs.msg import Float32
+import numpy as np
+
+pub = rospy.Publisher('random_float_log', Float32, queue_size=10)
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I head %s", data.data)
+    rospy.loginfo("Natural Log: %s", np.log(data.data))
+    pub.publish(np.log(data.data))
+
+def listener():
+    rospy.init_node('simple_subscriber', anonymous = True)
+
+    rospy.Subscriber('my_random_float', Float32, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener() 
